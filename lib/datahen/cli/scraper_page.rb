2d65118594be13d20ac8181497a2d54aaaf76eef
Rony Sandoval <jr.potit@gmail.com>
2020-10-27 14:53:19 -0600

    FETCH-502 Add page_type filter to refetch/reparse

--- lib/datahen/cli/scraper_page.rb
+++ lib/datahen/cli/scraper_page.rb
@@ -99,16 +99,17 @@ module Datahen
 
       desc "refetch <scraper_name>", "Refetch Pages on a scraper's current job"
       long_desc <<-LONGDESC
-        Refetch pages in a scraper's current job. You need to specify either a --gid or --fetch-fail or --parse-fail or --status.\x5
+        Refetch pages in a scraper's current job. You need to specify either a --gid or --fetch-fail or --parse-fail or --status or --page-type.\x5
       LONGDESC
       option :gid, :aliases => :g, type: :string, desc: 'Refetch a specific GID'
       option :fetch_fail, type: :boolean, desc: 'Refetches only pages that fails fetching.'
       option :parse_fail, type: :boolean, desc: 'Refetches only pages that fails parsing.'
       option :status, type: :string, desc: 'Refetches only pages with a specific status.'
+      option :page_type, type: :string, desc: 'Refetches only pages with a specific page type.'
       option :job, :aliases => :j, type: :numeric, desc: 'Set a specific job ID'
       def refetch(scraper_name)
-        if !options.key?(:gid) && !options.key?(:fetch_fail) && !options.key?(:parse_fail) && !options.key?(:status)
-          puts "Must specify either a --gid, --fetch-fail, --parse-fail or --status"
+        if !options.key?(:gid) && !options.key?(:fetch_fail) && !options.key?(:parse_fail) && !options.key?(:status) && !options.key?(:page_type)
+          puts "Must specify either a --gid, --fetch-fail, --parse-fail, --status or --page-type"
           return
         end
 
@@ -123,15 +124,16 @@ module Datahen
 
       desc "reparse <scraper_name>", "Reparse Pages on a scraper's current job"
       long_desc <<-LONGDESC
-        Reparse pages in a scraper's current job. You need to specify either a --gid or --parse-fail or --status.\x5
+        Reparse pages in a scraper's current job. You need to specify either a --gid or --parse-fail or --status or --page-type.\x5
       LONGDESC
       option :gid, :aliases => :g, type: :string, desc: 'Reparse a specific GID'
       option :parse_fail, type: :boolean, desc: 'Reparse only pages that fails parsing.'
       option :status, type: :string, desc: 'Reparse only pages with a specific status.'
+      option :page_type, type: :string, desc: 'Refetches only pages with a specific page type.'
       option :job, :aliases => :j, type: :numeric, desc: 'Set a specific job ID'
       def reparse(scraper_name)
-        if !options.key?(:gid) && !options.key?(:parse_fail) && !options.key?(:status)
-          puts "Must specify either a --gid, --parse-fail or --status"
+        if !options.key?(:gid) && !options.key?(:parse_fail) && !options.key?(:status) && !options.key?(:page_type)
+          puts "Must specify either a --gid, --parse-fail, --status or --page-type"
           return
         end
 
